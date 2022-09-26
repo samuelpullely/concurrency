a4df819429013d6447cdb12296dbde86599f6487
Samuel Pullely <samuelpullely@hotmail.com>
2022-09-26 12:37:01 +0200

    Run numberForever function in multiple threads

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,9 +1,19 @@
 module MyLib where
 
+import Control.Concurrent
 import Control.Monad
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-numberForever :: Show a => a -> IO b
+numberForever :: Int -> IO b
 numberForever = forever . print
+
+seconds :: Int
+seconds = 1000000
+
+mainFunc :: IO ()
+mainFunc = do
+    mapM_ (forkIO . numberForever) [1..10]
+    threadDelay $ 5 * seconds
+    putStrLn "done"
